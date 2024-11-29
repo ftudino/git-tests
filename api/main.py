9f41f2672f2e72551edb569da477fe0966202279
Federico Tudino <federico.tudino@markethub.com.ar>
2024-11-29 16:07:56 -0300

    add products

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from items import items_router
+from products import products_router
 
 app = FastAPI()
 
@@ -9,3 +10,4 @@ def read_root():
     return {"Hello": "World"}
 
 app.include_router(items_router)
+app.include_router(products_router)
