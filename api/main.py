c402db264fb9b00f23cf7dc6714fc2d3c31f1b67
Federico Tudino <federico.tudino@markethub.com.ar>
2024-11-29 15:32:13 -0300

    add basic router

--- api/main.py
+++ api/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
+router = APIRouter()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@router.get("/items/")
+def read_items():
+    return {"items": "items"}
+
+app.include_router(router)
